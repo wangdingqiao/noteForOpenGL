3e7ecc0f335e8c76497c479c5be702b52bfa8790
Wang Dingqiao <393524628@qq.com>
2016-08-14 19:34:26 +0800

    模板缓冲(stencil buffer)-outline

---
+++ stencilTesting/objectOutline/stencilTest.cpp
@@ -0,0 +1,339 @@
+// 引入GLEW库 定义静态链接
+#define GLEW_STATIC
+#include <GLEW/glew.h>
+// 引入GLFW库
+#include <GLFW/glfw3.h>
+// 引入SOIL库
+#include <SOIL/SOIL.h>
+// 引入GLM库
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+// 包含着色器加载库
+#include "shader.h"
+// 包含相机控制辅助类
+#include "camera.h"
+// 包含纹理加载类
+#include "texture.h"
+
+// 键盘回调函数原型声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+// 鼠标移动回调函数原型声明
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+// 鼠标滚轮回调函数原型声明
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// 场景中移动
+void do_movement();
+
+// 定义程序常量
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+// 用于相机交互参数
+GLfloat lastX = WINDOW_WIDTH / 2.0f, lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouseMove = true;
+bool keyPressedStatus[1024]; // 按键情况记录
+GLfloat deltaTime = 0.0f; // 当前帧和上一帧的时间差
+GLfloat lastFrame = 0.0f; // 上一帧时间
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+int main(int argc, char** argv)
+{
+	
+	if (!glfwInit())	// 初始化glfw库
+	{
+		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	// 开启OpenGL 3.3 core profile
+	std::cout << "Start OpenGL core profile version 3.3" << std::endl;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+		"Demo of outline(using stencil buffer)", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Error::GLFW could not create winddow!" << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+	// 创建的窗口的context指定为当前context
+	glfwMakeContextCurrent(window);
+
+	// 注册窗口键盘事件回调函数
+	glfwSetKeyCallback(window, key_callback);
+	// 注册鼠标事件回调函数
+	glfwSetCursorPosCallback(window, mouse_move_callback);
+	// 注册鼠标滚轮事件回调函数
+	glfwSetScrollCallback(window, mouse_scroll_callback);
+	// 鼠标捕获 停留在程序内
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// 初始化GLEW 获取OpenGL函数
+	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
+	{
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION) 
+			<< " error string:" << glewGetErrorString(status) << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+
+	// 设置视口参数
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	//Section1 顶点属性数据
+	// 指定立方体顶点属性数据 顶点位置 颜色 纹理
+	GLfloat cubeVertices[] = {
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// A
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	// B
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+
+
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, 0.5f, -0.5f,1.0, 1.0f,   // H
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// B
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		0.5f, 0.5f, 0.5f,1.0f, 0.0f,	// C
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,// E
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, 0.5f,1.0f, 0.0f,	// B
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+	};
+	// 平面的顶点属性数据 顶点位置 纹理坐标(设置的值大于1.0用于重复)
+	GLfloat planeVertices[] = {
+		5.0f, -0.5f, 5.0f, 2.0f, 0.0f,   // A
+		5.0f, -0.5f, -5.0f, 2.0f, 2.0f,  // D
+		-5.0f, -0.5f, -5.0f, 0.0f, 2.0f, // C
+
+		-5.0f, -0.5f, -5.0f, 0.0f, 2.0f, // C
+		-5.0f, -0.5f, 5.0f, 0.0f, 0.0f,  // B
+		5.0f, -0.5f, 5.0f, 2.0f, 0.0f,   // A
+	};
+	// Section2 准备缓存对象
+	GLuint cubeVAOId, cubeVBOId;
+	glGenVertexArrays(1, &cubeVAOId);
+	glGenBuffers(1, &cubeVBOId);
+	glBindVertexArray(cubeVAOId);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBOId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	// 顶点位置数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
+		5 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 顶点纹理数据
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 
+		5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	GLuint planeVAOId, planeVBOId;
+	glGenVertexArrays(1, &planeVAOId);
+	glGenBuffers(1, &planeVBOId);
+	glBindVertexArray(planeVAOId);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBOId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+	// 顶点位置数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+		5 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 顶点纹理数据
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
+		5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	// Section3 加载纹理
+	GLuint cubeTextId = TextureHelper::load2DTexture("../../resources/textures/marble.jpg");
+	GLuint planeTextId = TextureHelper::load2DTexture("../../resources/textures/metal.png");
+	// Section4 准备着色器程序
+	Shader shader("stencilTest.vertex", "stencilTest.frag");
+	Shader outlineShader("singleColor.vertex", "singleColor.frag");
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_STENCIL_TEST);
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE); // 在模板测试和深度测试都通过时更新模板缓冲区
+	// 开始游戏主循环
+	while (!glfwWindowShouldClose(window))
+	{
+		GLfloat currentFrame = (GLfloat)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		glfwPollEvents(); // 处理例如鼠标 键盘等事件
+		do_movement(); // 根据用户操作情况 更新相机属性
+
+		// 清除颜色缓冲区 深度缓冲区 模板缓冲区
+		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		glm::mat4 projection = glm::perspective(camera.mouse_zoom,
+			(GLfloat)(WINDOW_WIDTH) / WINDOW_HEIGHT, 1.0f, 100.0f); // 投影矩阵
+		glm::mat4 view = camera.getViewMatrix(); // 视变换矩阵
+		
+		shader.use();
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "projection"),
+			1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "view"),
+			1, GL_FALSE, glm::value_ptr(view));
+		outlineShader.use();
+		glUniformMatrix4fv(glGetUniformLocation(outlineShader.programId, "projection"),
+			1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(outlineShader.programId, "view"),
+			1, GL_FALSE, glm::value_ptr(view));
+		
+		// 绘制地板 这里不使用模板
+		shader.use();
+		glStencilMask(0x00);
+		glBindVertexArray(planeVAOId);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, planeTextId);
+		glm::mat4 model = glm::mat4();
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// 绘制原始立方体 这里开启模板
+		shader.use();
+		glStencilMask(0xFF);
+		glStencilFunc(GL_ALWAYS, 1, 0xFF);
+		glBindVertexArray(cubeVAOId);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTextId);
+		// 绘制第一个立方体
+		model = glm::mat4();
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		// 绘制第二个立方体
+		model = glm::mat4();
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		//	使用模板缓冲区 绘制立方体边缘
+		outlineShader.use();
+		glStencilMask(0x00); // 禁止写入模板缓冲区
+		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+		glDisable(GL_DEPTH_TEST); // 这里关闭深度测试 是为了让轮廓不因为处在前面的平面而被消去
+		const GLfloat scale = 1.1f;
+		glBindVertexArray(cubeVAOId);
+		// 绘制第一个立方体轮廓
+		model = glm::mat4();
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		model = glm::scale(model, glm::vec3(scale, scale, scale));
+		glUniformMatrix4fv(glGetUniformLocation(outlineShader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36); // 这里的36是用于构成三角形的顶点个数 而不是三角形的个数12
+		// 绘制第二个立方体轮廓
+		model = glm::mat4();
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(scale, scale, scale));
+		glUniformMatrix4fv(glGetUniformLocation(outlineShader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glEnable(GL_DEPTH_TEST);
+		glStencilMask(0xFF); // 注意这里要再次允许对模板缓冲进行写入，否则下次进入循环时无法清除模板缓冲
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glfwSwapBuffers(window); // 交换缓存
+	}
+	// 释放资源
+	glDeleteVertexArrays(1, &cubeVAOId);
+	glDeleteVertexArrays(1, &planeVAOId);
+	glDeleteBuffers(1, &cubeVBOId);
+	glDeleteBuffers(1, &planeVBOId);
+	glfwTerminate();
+	return 0;
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keyPressedStatus[key] = true;
+		else if (action == GLFW_RELEASE)
+			keyPressedStatus[key] = false;
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+	}
+}
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouseMove) // 首次鼠标移动
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouseMove = false; 
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.handleMouseMove(xoffset, yoffset);
+}
+// 由相机辅助类处理鼠标滚轮控制
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.handleMouseScroll(yoffset);
+}
+// 由相机辅助类处理键盘控制
+void do_movement()
+{
+	
+	if (keyPressedStatus[GLFW_KEY_W])
+		camera.handleKeyPress(FORWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_S])
+		camera.handleKeyPress(BACKWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_A])
+		camera.handleKeyPress(LEFT, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_D])
+		camera.handleKeyPress(RIGHT, deltaTime);
+}
