666aa27cb3553032af44ffe19021ccb8a1ea7ee7
Wang Dingqiao <393524628@qq.com>
2016-09-11 18:19:46 +0800

    cubemap-skybox-使相机位于观察者位置实现

---
+++ skyBox/skyBoxUsingScale/skyBoxUsingScale.cpp
@@ -0,0 +1,359 @@
+// 引入GLEW库 定义静态链接
+#define GLEW_STATIC
+#include <GLEW/glew.h>
+// 引入GLFW库
+#include <GLFW/glfw3.h>
+// 引入SOIL库
+#include <SOIL/SOIL.h>
+// 引入GLM库
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+// 包含着色器加载库
+#include "shader.h"
+// 包含相机控制辅助类
+#include "camera.h"
+// 包含纹理加载类
+#include "texture.h"
+
+// 键盘回调函数原型声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+// 鼠标移动回调函数原型声明
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+// 鼠标滚轮回调函数原型声明
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// 场景中移动
+void do_movement();
+
+// 定义程序常量
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+// 用于相机交互参数
+GLfloat lastX = WINDOW_WIDTH / 2.0f, lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouseMove = true;
+bool keyPressedStatus[1024]; // 按键情况记录
+GLfloat deltaTime = 0.0f; // 当前帧和上一帧的时间差
+GLfloat lastFrame = 0.0f; // 上一帧时间
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+int main(int argc, char** argv)
+{
+	
+	if (!glfwInit())	// 初始化glfw库
+	{
+		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	// 开启OpenGL 3.3 core profile
+	std::cout << "Start OpenGL core profile version 3.3" << std::endl;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+		"Demo of Skybox", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Error::GLFW could not create winddow!" << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+	// 创建的窗口的context指定为当前context
+	glfwMakeContextCurrent(window);
+
+	// 注册窗口键盘事件回调函数
+	glfwSetKeyCallback(window, key_callback);
+	// 注册鼠标事件回调函数
+	glfwSetCursorPosCallback(window, mouse_move_callback);
+	// 注册鼠标滚轮事件回调函数
+	glfwSetScrollCallback(window, mouse_scroll_callback);
+	// 鼠标捕获 停留在程序内
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// 初始化GLEW 获取OpenGL函数
+	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
+	{
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION) 
+			<< " error string:" << glewGetErrorString(status) << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+
+	// 设置视口参数
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	//Section1 顶点属性数据
+	// 指定立方体顶点属性数据 顶点位置 纹理
+	GLfloat cubeVertices[] = {
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// A
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	// B
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+
+
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, 0.5f, -0.5f,1.0, 1.0f,   // H
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// B
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		0.5f, 0.5f, 0.5f,1.0f, 0.0f,	// C
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,// E
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, 0.5f,1.0f, 0.0f,	// B
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+	};
+	// 指定包围盒的顶点属性 位置
+	GLfloat skyboxVertices[] = {
+		// 背面
+		-1.0f, 1.0f, -1.0f,		// A
+		-1.0f, -1.0f, -1.0f,	// B
+		1.0f, -1.0f, -1.0f,		// C
+		1.0f, -1.0f, -1.0f,		// C
+		1.0f, 1.0f, -1.0f,		// D
+		-1.0f, 1.0f, -1.0f,		// A
+
+		// 左侧面
+		-1.0f, -1.0f, 1.0f,		// E
+		-1.0f, -1.0f, -1.0f,	// B
+		-1.0f, 1.0f, -1.0f,		// A
+		-1.0f, 1.0f, -1.0f,		// A
+		-1.0f, 1.0f, 1.0f,		// F
+		-1.0f, -1.0f, 1.0f,		// E
+
+		// 右侧面
+		1.0f, -1.0f, -1.0f,		// C
+		1.0f, -1.0f, 1.0f,		// G
+		1.0f, 1.0f, 1.0f,		// H
+		1.0f, 1.0f, 1.0f,		// H
+		1.0f, 1.0f, -1.0f,		// D
+		1.0f, -1.0f, -1.0f,		// C
+
+		// 正面
+		-1.0f, -1.0f, 1.0f,  // E
+		-1.0f, 1.0f, 1.0f,  // F
+		1.0f, 1.0f, 1.0f,  // H
+		1.0f, 1.0f, 1.0f,  // H
+		1.0f, -1.0f, 1.0f,  // G
+		-1.0f, -1.0f, 1.0f,  // E
+
+		// 顶面
+		-1.0f, 1.0f, -1.0f,  // A
+		1.0f, 1.0f, -1.0f,  // D
+		1.0f, 1.0f, 1.0f,  // H
+		1.0f, 1.0f, 1.0f,  // H
+		-1.0f, 1.0f, 1.0f,  // F
+		-1.0f, 1.0f, -1.0f,  // A
+
+		// 底面
+		-1.0f, -1.0f, -1.0f,  // B
+		-1.0f, -1.0f, 1.0f,   // E
+		1.0f, -1.0f, 1.0f,    // G
+		1.0f, -1.0f, 1.0f,    // G
+		1.0f, -1.0f, -1.0f,   // C
+		-1.0f, -1.0f, -1.0f,  // B
+	};
+
+
+	// Section2 准备缓存对象
+	GLuint cubeVAOId, cubeVBOId;
+	glGenVertexArrays(1, &cubeVAOId);
+	glGenBuffers(1, &cubeVBOId);
+	glBindVertexArray(cubeVAOId);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBOId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	// 顶点位置数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
+		5 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 顶点纹理数据
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 
+		5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	GLuint skyBoxVAOId, skyBoxVBOId;
+	glGenVertexArrays(1, &skyBoxVAOId);
+	glGenBuffers(1, &skyBoxVBOId);
+	glBindVertexArray(skyBoxVAOId);
+	glBindBuffer(GL_ARRAY_BUFFER, skyBoxVBOId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+	// 顶点位置数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+		3 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	// Section3 加载纹理
+	GLuint cubeTextId = TextureHelper::load2DTexture("../../resources/textures/container.jpg");
+	std::vector<const char*> faces;
+// 	faces.push_back("../../resources/skyboxes/sky/sky_rt.jpg");
+// 	faces.push_back("../../resources/skyboxes/sky/sky_lf.jpg");
+// 	faces.push_back("../../resources/skyboxes/sky/sky_up.jpg");
+// 	faces.push_back("../../resources/skyboxes/sky/sky_dn.jpg");
+// 	faces.push_back("../../resources/skyboxes/sky/sky_bk.jpg");
+// 	faces.push_back("../../resources/skyboxes/sky/sky_ft.jpg");
+// 
+	faces.push_back("../../resources/skyboxes/urbansp/urbansp_rt.tga");
+	faces.push_back("../../resources/skyboxes/urbansp/urbansp_lf.tga");
+	faces.push_back("../../resources/skyboxes/urbansp/urbansp_up.tga");
+	faces.push_back("../../resources/skyboxes/urbansp/urbansp_dn.tga");
+	faces.push_back("../../resources/skyboxes/urbansp/urbansp_bk.tga");
+	faces.push_back("../../resources/skyboxes/urbansp/urbansp_ft.tga");
+	GLuint skyBoxTextId = TextureHelper::loadCubeMapTexture(faces);
+	// Section4 准备着色器程序
+	Shader shader("scene.vertex", "scene.frag");
+	Shader skyBoxShader("skybox.vertex", "skybox.frag");
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glDepthFunc(GL_LESS);
+	// 开始游戏主循环
+	while (!glfwWindowShouldClose(window))
+	{
+		GLfloat currentFrame = (GLfloat)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		glfwPollEvents(); // 处理例如鼠标 键盘等事件
+		do_movement(); // 根据用户操作情况 更新相机属性
+
+		// 设置colorBuffer颜色
+		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
+		// 清除colorBuffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// 先绘制场景
+		shader.use();
+		glm::mat4 projection = glm::perspective(camera.mouse_zoom,
+			(GLfloat)(WINDOW_WIDTH) / WINDOW_HEIGHT, 0.1f, 100.0f); // 投影矩阵
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "projection"),
+			1, GL_FALSE, glm::value_ptr(projection));
+		glm::mat4 view = camera.getViewMatrix(); // 视变换矩阵
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "view"),
+			1, GL_FALSE, glm::value_ptr(view));
+		glm::mat4 model;
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(cubeVAOId);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTextId);
+		glUniform1i(glGetUniformLocation(skyBoxShader.programId, "text"), 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// 然后绘制包围盒  使包围盒原点位于观察者位置并使用缩放
+		glDepthFunc(GL_LEQUAL); // 深度测试条件 小于等于
+		skyBoxShader.use();
+		glUniformMatrix4fv(glGetUniformLocation(skyBoxShader.programId, "projection"),
+			1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(skyBoxShader.programId, "view"),
+			1, GL_FALSE, glm::value_ptr(view));
+		model = glm::translate(glm::mat4(), camera.position);
+		model = glm::scale(model, glm::vec3(20.0f, 20.0f, 20.0f));
+		glUniformMatrix4fv(glGetUniformLocation(skyBoxShader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(skyBoxVAOId);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, skyBoxTextId); // 注意绑定到CUBE_MAP
+		glUniform1i(glGetUniformLocation(skyBoxShader.programId, "skybox"), 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glDepthFunc(GL_LESS);
+		glfwSwapBuffers(window); // 交换缓存
+	}
+	// 释放资源
+	glDeleteVertexArrays(1, &cubeVAOId);
+	glDeleteVertexArrays(1, &skyBoxVAOId);
+	glDeleteBuffers(1, &cubeVBOId);
+	glDeleteBuffers(1, &skyBoxVBOId);
+	glfwTerminate();
+	return 0;
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keyPressedStatus[key] = true;
+		else if (action == GLFW_RELEASE)
+			keyPressedStatus[key] = false;
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+	}
+}
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouseMove) // 首次鼠标移动
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouseMove = false; 
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.handleMouseMove(xoffset, yoffset);
+}
+// 由相机辅助类处理鼠标滚轮控制
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.handleMouseScroll(yoffset);
+}
+// 由相机辅助类处理键盘控制
+void do_movement()
+{
+	
+	if (keyPressedStatus[GLFW_KEY_W])
+		camera.handleKeyPress(FORWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_S])
+		camera.handleKeyPress(BACKWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_A])
+		camera.handleKeyPress(LEFT, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_D])
+		camera.handleKeyPress(RIGHT, deltaTime);
+}
