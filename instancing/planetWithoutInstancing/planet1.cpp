238ed650ed42a319ccb9b493efe4085b8f1c5f52
Wang Dingqiao <393524628@qq.com>
2016-10-04 11:22:46 +0800

    instancing-不使用instance array绘制行星带

---
+++ instancing/planetWithoutInstancing/planet1.cpp
@@ -0,0 +1,267 @@
+// 引入GLEW库 定义静态链接
+#define GLEW_STATIC
+#include <GLEW/glew.h>
+// 引入GLFW库
+#include <GLFW/glfw3.h>
+// 引入SOIL库
+#include <SOIL/SOIL.h>
+// 引入GLM库
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+// 包含着色器加载库
+#include "shader.h"
+// 包含相机控制辅助类
+#include "camera.h"
+// 包含纹理加载类
+#include "texture.h"
+// 加载模型的类
+#include "model.h"
+
+// 键盘回调函数原型声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+// 鼠标移动回调函数原型声明
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+// 鼠标滚轮回调函数原型声明
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// 场景中移动
+void do_movement();
+
+void prepareInstanceMatrices(std::vector<glm::mat4>& modelMatrices, const int amount);
+
+// 定义程序常量
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+// 用于相机交互参数
+GLfloat lastX = WINDOW_WIDTH / 2.0f, lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouseMove = true;
+bool keyPressedStatus[1024]; // 按键情况记录
+GLfloat deltaTime = 0.0f; // 当前帧和上一帧的时间差
+GLfloat lastFrame = 0.0f; // 上一帧时间
+Camera camera(glm::vec3(0.0f, 0.0f, 55.0f));
+const int INSTANCE_COUNT = 10000;
+
+int main(int argc, char** argv)
+{
+	
+	if (!glfwInit())	// 初始化glfw库
+	{
+		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	// 开启OpenGL 3.3 core profile
+	std::cout << "Start OpenGL core profile version 3.3" << std::endl;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+		"Demo of instancing model", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Error::GLFW could not create winddow!" << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+	// 创建的窗口的context指定为当前context
+	glfwMakeContextCurrent(window);
+
+	// 注册窗口键盘事件回调函数
+	glfwSetKeyCallback(window, key_callback);
+	// 注册鼠标事件回调函数
+	glfwSetCursorPosCallback(window, mouse_move_callback);
+	// 注册鼠标滚轮事件回调函数
+	glfwSetScrollCallback(window, mouse_scroll_callback);
+	// 鼠标捕获 停留在程序内
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// 初始化GLEW 获取OpenGL函数
+	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
+	{
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION) 
+			<< " error string:" << glewGetErrorString(status) << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+
+	// 设置视口参数
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	//Section1 加载模型数据 为了方便更换模型 我们从文件读取模型文件路径
+	Model objModels[2];
+	std::ifstream modelPath("modelPath.txt");
+	if (!modelPath)
+	{
+		std::cerr << "Error::could not read model path file." << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+	std::string modelFilePath;
+	for (int i = 0; i < 2; ++i)
+	{
+		std::getline(modelPath, modelFilePath);
+		if (modelFilePath.empty())
+		{
+			std::cerr << "Error::model path empty." << std::endl;
+			glfwTerminate();
+			std::system("pause");
+			return -1;
+		}
+		if (!objModels[i].loadModel(modelFilePath))
+		{
+			glfwTerminate();
+			std::system("pause");
+			return -1;
+		}
+	}
+	Model& planet = objModels[0];
+	Model& rock = objModels[1];
+	// Section2 准备着色器程序
+	Shader shader("scene.vertex", "scene.frag");
+
+	// Section3 为多个实例准备模型变换矩阵
+	std::vector<glm::mat4> modelMatrices;
+	prepareInstanceMatrices(modelMatrices, INSTANCE_COUNT);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	// 开始游戏主循环
+	while (!glfwWindowShouldClose(window))
+	{
+		GLfloat currentFrame = (GLfloat)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		std::cout << " time used in this frame: " << deltaTime << " seconds." << std::endl;
+		glfwPollEvents(); // 处理例如鼠标 键盘等事件
+		do_movement(); // 根据用户操作情况 更新相机属性
+
+		// 清除颜色缓冲区 重置为指定颜色
+		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// 这里填写场景绘制代码
+		shader.use();
+		glm::mat4 projection = glm::perspective(camera.mouse_zoom,
+			(GLfloat)(WINDOW_WIDTH) / WINDOW_HEIGHT, 1.0f, 10000.0f); // 投影矩阵
+		glm::mat4 view = camera.getViewMatrix(); // 视变换矩阵
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "projection"),
+			1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "view"),
+			1, GL_FALSE, glm::value_ptr(view));
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		
+		planet.draw(shader); // 先绘制行星
+
+		// 绘制多个小行星实例
+		for (std::vector<glm::mat4>::size_type i = 0; i < modelMatrices.size(); ++i)
+		{
+			glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+				1, GL_FALSE, glm::value_ptr(modelMatrices[i]));
+			rock.draw(shader);
+		}
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glfwSwapBuffers(window); // 交换缓存
+	}
+	// 释放资源
+	glfwTerminate();
+	return 0;
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keyPressedStatus[key] = true;
+		else if (action == GLFW_RELEASE)
+			keyPressedStatus[key] = false;
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+	}
+}
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouseMove) // 首次鼠标移动
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouseMove = false; 
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.handleMouseMove(xoffset, yoffset);
+}
+// 由相机辅助类处理鼠标滚轮控制
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.handleMouseScroll(yoffset);
+}
+// 由相机辅助类处理键盘控制
+void do_movement()
+{
+	
+	if (keyPressedStatus[GLFW_KEY_W])
+		camera.handleKeyPress(FORWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_S])
+		camera.handleKeyPress(BACKWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_A])
+		camera.handleKeyPress(LEFT, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_D])
+		camera.handleKeyPress(RIGHT, deltaTime);
+}
+// 为实例准备模型变换矩阵
+// 这里的细节可以不用深究
+void prepareInstanceMatrices(std::vector<glm::mat4>& modelMatrices, const int amount)
+{
+	srand(glfwGetTime()); // 初始化随机数的种子
+	GLfloat radius = 50.0;
+	GLfloat offset = 2.5f;
+	for (GLuint i = 0; i < amount; i++)
+	{
+		glm::mat4 model;
+		// 1. 平移
+		GLfloat angle = (GLfloat)i / (GLfloat)amount * 360.0f;
+		GLfloat displacement = (rand() % (GLint)(2 * offset * 100)) / 100.0f - offset;
+		GLfloat x = sin(angle) * radius + displacement;
+		displacement = (rand() % (GLint)(2 * offset * 100)) / 100.0f - offset;
+		GLfloat y = displacement * 0.4f; 
+		displacement = (rand() % (GLint)(2 * offset * 100)) / 100.0f - offset;
+		GLfloat z = cos(angle) * radius + displacement;
+		model = glm::translate(model, glm::vec3(x, y, z));
+
+		// 2. 缩放 在 0.05 和 0.25f 之间
+		GLfloat scale = (rand() % 20) / 100.0f + 0.05;
+		model = glm::scale(model, glm::vec3(scale));
+
+		// 3. 旋转
+		GLfloat rotAngle = (rand() % 360);
+		model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
+
+		// 4. 添加作为模型变换矩阵
+		modelMatrices.push_back(model);
+	}
+}
