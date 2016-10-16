77341313932a10484cae9270a7667e151139b52b
Wang Dingqiao <393524628@qq.com>
2016-10-16 12:07:51 +0800

    反走样-使用多采样的FBO

---
+++ antiAliasing/antialiasingFBO/cube.cpp
@@ -0,0 +1,305 @@
+// 引入GLEW库 定义静态链接
+#define GLEW_STATIC
+#include <GLEW/glew.h>
+// 引入GLFW库
+#include <GLFW/glfw3.h>
+// 引入SOIL库
+#include <SOIL/SOIL.h>
+// 引入GLM库
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+// 包含着色器加载库
+#include "shader.h"
+// 包含相机控制辅助类
+#include "camera.h"
+// 包含纹理加载类
+#include "texture.h"
+
+// 键盘回调函数原型声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+// 鼠标移动回调函数原型声明
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+// 鼠标滚轮回调函数原型声明
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// 场景中移动
+void do_movement();
+
+bool prepareMSFBO(const int samplesCnt, GLuint& textId, GLuint& fboId);
+
+// 定义程序常量
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+const int SAMPLES_COUNT = 4;
+// 用于相机交互参数
+GLfloat lastX = WINDOW_WIDTH / 2.0f, lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouseMove = true;
+bool keyPressedStatus[1024]; // 按键情况记录
+GLfloat deltaTime = 0.0f; // 当前帧和上一帧的时间差
+GLfloat lastFrame = 0.0f; // 上一帧时间
+Camera camera(glm::vec3(0.0f, 0.0f, 2.0f));
+
+int main(int argc, char** argv)
+{
+	
+	if (!glfwInit())	// 初始化glfw库
+	{
+		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	// 开启OpenGL 3.3 core profile
+	std::cout << "Start OpenGL core profile version 3.3" << std::endl;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+		"Demo of multiSampled FBO", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Error::GLFW could not create winddow!" << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+	// 创建的窗口的context指定为当前context
+	glfwMakeContextCurrent(window);
+
+	// 注册窗口键盘事件回调函数
+	glfwSetKeyCallback(window, key_callback);
+	// 注册鼠标事件回调函数
+	glfwSetCursorPosCallback(window, mouse_move_callback);
+	// 注册鼠标滚轮事件回调函数
+	glfwSetScrollCallback(window, mouse_scroll_callback);
+	// 鼠标捕获 停留在程序内
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// 初始化GLEW 获取OpenGL函数
+	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
+	{
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION) 
+			<< " error string:" << glewGetErrorString(status) << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+
+	// 设置视口参数
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	//Section1 顶点属性数据
+	// 指定立方体顶点属性数据 顶点位置 纹理
+	GLfloat cubeVertices[] = {
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// A
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	// B
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+
+
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, 0.5f, -0.5f,1.0, 1.0f,   // H
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// B
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		0.5f, 0.5f, 0.5f,1.0f, 0.0f,	// C
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,// E
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, 0.5f,1.0f, 0.0f,	// B
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+	};
+	// Section2 准备缓存对象
+	GLuint cubeVAOId, cubeVBOId;
+	glGenVertexArrays(1, &cubeVAOId);
+	glGenBuffers(1, &cubeVBOId);
+	glBindVertexArray(cubeVAOId);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBOId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	// 顶点位置数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
+		5 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 顶点纹理数据
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 
+		5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	// Section3 准备着色器程序
+	Shader shader("scene.vertex", "scene.frag");
+	
+	// Section4 创建多采样的FBO
+	GLuint MSTextId, MSFBOId;
+	if (!prepareMSFBO(SAMPLES_COUNT, MSTextId, MSFBOId))
+	{
+		std::cout << "Error::FBO :" << " not complete." << std::endl;
+		glfwTerminate();
+		std::system("pause");
+		return -1;
+	}
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_CULL_FACE);
+	// 开始游戏主循环
+	while (!glfwWindowShouldClose(window))
+	{
+		GLfloat currentFrame = (GLfloat)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		glfwPollEvents(); // 处理例如鼠标 键盘等事件
+		do_movement(); // 根据用户操作情况 更新相机属性
+
+		// 第一遍 绘制到多采样的FBO中
+		glBindFramebuffer(GL_FRAMEBUFFER, MSFBOId);
+		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.use();
+		glm::mat4 projection = glm::perspective(camera.mouse_zoom,
+			(GLfloat)(WINDOW_WIDTH) / WINDOW_HEIGHT, 1.0f, 100.0f); // 投影矩阵
+		glm::mat4 view = camera.getViewMatrix(); // 视变换矩阵
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "projection"),
+			1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "view"),
+			1, GL_FALSE, glm::value_ptr(view));
+		glm::mat4 model;
+		model = glm::mat4();
+		model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"),
+			1, GL_FALSE, glm::value_ptr(model));
+		// 这里填写场景绘制代码
+		glBindVertexArray(cubeVAOId);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+
+		// 第二遍 将多采样的FBO纹理复制到默认FBO
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, MSFBOId);
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);  // 这里0表示使用默认的FBO
+		glBlitFramebuffer(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 
+			0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glfwSwapBuffers(window); // 交换缓存
+	}
+	// 释放资源
+	glDeleteVertexArrays(1, &cubeVAOId);
+	glDeleteBuffers(1, &cubeVBOId);
+	glfwTerminate();
+	return 0;
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keyPressedStatus[key] = true;
+		else if (action == GLFW_RELEASE)
+			keyPressedStatus[key] = false;
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+	}
+	else if (key == GLFW_KEY_O && action == GLFW_PRESS)
+	{
+		glEnable(GL_MULTISAMPLE);
+	}
+	else if (key == GLFW_KEY_F && action == GLFW_PRESS)
+	{
+		glDisable(GL_MULTISAMPLE);
+	}
+}
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouseMove) // 首次鼠标移动
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouseMove = false; 
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.handleMouseMove(xoffset, yoffset);
+}
+// 由相机辅助类处理鼠标滚轮控制
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.handleMouseScroll(yoffset);
+}
+// 由相机辅助类处理键盘控制
+void do_movement()
+{
+	
+	if (keyPressedStatus[GLFW_KEY_W])
+		camera.handleKeyPress(FORWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_S])
+		camera.handleKeyPress(BACKWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_A])
+		camera.handleKeyPress(LEFT, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_D])
+		camera.handleKeyPress(RIGHT, deltaTime);
+}
+/*
+* 附加纹理到Color Attachment 同时附加RBO到depth stencil Attachment
+*/
+bool prepareMSFBO(const int samplesCnt, GLuint& colorTextId, GLuint& fboId)
+{
+	glGenFramebuffers(1, &fboId);
+	glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+	// 附加 color attachment
+	colorTextId = TextureHelper::makeMAAttachmentTexture(samplesCnt, GL_RGB); // 创建FBO中的多采样纹理
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, colorTextId, 0);
+	// 附加 depth stencil RBO attachment
+	GLuint rboId;
+	glGenRenderbuffers(1, &rboId);
+	glBindRenderbuffer(GL_RENDERBUFFER, rboId);
+	glRenderbufferStorageMultisample(GL_RENDERBUFFER, samplesCnt, GL_DEPTH24_STENCIL8,
+		WINDOW_WIDTH, WINDOW_HEIGHT); // 预分配内存
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rboId);
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		return false;
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	return true;
+}
